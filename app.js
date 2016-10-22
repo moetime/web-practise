79dbc9ca7eb387f3906fe59a362d7eb038943eec
moetime <morrice_pfeiffer@hotmail.com>
2016-10-22 05:44:36 -0400

    commit 5

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+// var comments to be reemoved 
+
 var app = express();
 
 // view engine setup
